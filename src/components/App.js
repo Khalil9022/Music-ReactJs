ddce92f4132f4d05a42c04b17f6aa6ed3512bb72
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-09-10 10:27:30 +0700

    adding search button to search song

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>React App </div>
-  );
+class App extends Component {
+  state = { artistQuery: '' }
+
+  updateArtistQuery = event => {
+    console.log('event.target.value', event.target.value);
+    this.setState({ artistQuery: event.target.value })
+  }
+
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.searchArtis()
+    }
+  }
+
+  searchArtis = () => {
+    console.log('this.state', this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <input
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder="Search for an Artist" />
+        <button onClick={this.searchArtis}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default App;
